9a8c171a01ca45442bb5fbe098e70926f8c02af6
Your Name <maloubastiaanse@hotmail.com>
2021-11-25 18:47:44 +0100

    Added class to write out data and a notebook to do data visualization in

--- queue_function.py
+++ queue_function.py
@@ -12,53 +12,64 @@ Scenario:
 
 """
 from numpy import random
-
 import simpy
 
 
-RANDOM_SEED = 42
-NEW_CUSTOMER = 100  # Total number of customers
-ARRIVAL_RATE = 10
-CAPACITY_SERVER = 10
-N_SERVERS = 1
-QUEING_DISCIPLINE = "FIFO"
+class QueueData:
+    def __init__(self):
+        self.times = []
+        self.wait_times = []
 
 
-def source(env, counter):
+def source(env, counter,  arrival_rate, capacity_server, data):
     """Source generates customers randomly"""
-    #for i in range(number):
+    # for i in range(number):
     i = 0
     while True:
-        c = customer(env, 'Customer%02d' % i, counter)
+        c = customer(env, 'Customer%02d' % i, counter, capacity_server, data)
         env.process(c)
-        t = random.exponential(scale=1/ARRIVAL_RATE, size=None)
+        t = random.exponential(scale=1/arrival_rate, size=None)
         yield env.timeout(t)
-        i +=1
+        i += 1
 
 
-def customer(env, name, counter):
+def customer(env, name, counter, capacity_server, data):
     """Customer arrives, is served and leaves."""
     arrive = env.now
-    print('%7.4f %s: Here I am' % (arrive, name))
+    #print('%7.4f %s: Here I am' % (arrive, name))
 
     with counter.request() as req:
         yield req
         wait = env.now - arrive
         # We got to the counter
-        print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+        #print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+        data.times.append(arrive)
+        data.wait_times.append(wait)
 
-        #time of service
-        tib = random.exponential(scale=1/CAPACITY_SERVER, size=None)
+        # time of service
+        tib = random.exponential(scale=1/capacity_server, size=None)
         yield env.timeout(tib)
-        print('%7.4f %s: Finished' % (env.now, name))
+        #print('%7.4f %s: Finished' % (env.now, name))
 
 
-# Setup and start the simulation
-print('DES start')
-random.seed(RANDOM_SEED)
-env = simpy.Environment()
+def run_queue_experiment(
+        random_seed,
+        time,
+        arrival_rate,
+        capacity_server,
+        n_server=1,
+        queueing_discipline="FIFO"):
+    data = QueueData()
 
-# Start processes and run
-counter = simpy.Resource(env, capacity=N_SERVERS)
-env.process(source(env, counter))
-env.run(until=15)
+    # Setup and start the simulation
+    print('DES start')
+    print(queueing_discipline)
+    random.seed(random_seed)
+    env = simpy.Environment()
+
+    # Start processes and run
+    counter = simpy.Resource(env, capacity=n_server)
+    env.process(source(env, counter, arrival_rate, capacity_server, data))
+    env.run(until=time)
+
+    return data
